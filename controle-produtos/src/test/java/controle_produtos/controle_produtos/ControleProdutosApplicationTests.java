8f2539f7769a379ec003b3af5297a4190edb0953
Claudemir84 <claudemirdias84@gmail.com>
2025-10-29 20:44:01 -0300

    Semana2

---
+++ controle-produtos/src/test/java/controle_produtos/controle_produtos/ControleProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package controle_produtos.controle_produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
